9f985071d7152a8b42c48f797dfd749383007a3a
andrik154 <a.u.litovets@gmail.com>
2021-07-06 17:08:48 +0300

    ..

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const log = require('./logger.js').log; //custom logger xd
 const tf = require('@tensorflow/tfjs-node');
 
 //code
-
 var ok_unsorted = fs.readdirSync('./ok');
 var ok = [];
 
@@ -12,15 +11,11 @@ var not_ok_unsorted = fs.readdirSync('./not_ok');
 var not_ok = [];
 
 //do a little trolling...
-
 var img_buffer = fs.readFileSync('./not_ok/test.jpg');
 var img_tensor = tf.node.decodeImage(img_buffer);
 log(0,img_tensor);
 
-const model = tf.sequential();
-model.add(tf.layers.dense({units: 100, activation: 'relu', inputShape: [10]}));
 ///sfasdfasdfs
-
 (async()=>{
     await ok_unsorted.forEach(e=>{
         if(/(.+).png/.test(e)||/(.+).jpg/.test(e)||/(.+).jpeg/.test(e)){
@@ -35,6 +30,3 @@ model.add(tf.layers.dense({units: 100, activation: 'relu', inputShape: [10]}));
     console.log(ok);
     console.log(not_ok);
 })();
-
-
-
