0945f9da7d29f1899136448e7de651b23b2a637d
andrik154 <a.u.litovets@gmail.com>
2021-07-06 22:56:23 +0300

    rename index.js, little trolling

--- input.js
+++ input.js
@@ -5,28 +5,38 @@ const tf = require('@tensorflow/tfjs-node');
 
 //do a little trolling...
 
-//tezt 
-var img_buffer = fs.readFileSync('./not_ok/test.jpg');
+//tezt  
+/*var img_buffer = fs.readFileSync('./not_ok/test.jpg');
 var img_tensor = tf.node.decodeImage(img_buffer);
 log(2, img_tensor.shape);
 var img_tensor2 = tf.image.resizeNearestNeighbor(img_tensor, [100,100]);
 log(2, img_tensor2.shape);
-log(0,img_tensor);
+log(0,img_tensor);*/
 
 //A LITTLE TROLLING
 async function loadImages(dir){
     var unsorted = fs.readdirSync(dir);
     var sorted = [];
+    var tensors=[];
     await unsorted.forEach(e=>{
         if(/(.+).png/.test(e)||/(.+).jpg/.test(e)||/(.+).jpeg/.test(e)){
             sorted.push(e);
         }
     });
-    return sorted
+    await sorted.forEach(e=>{
+        let img_buffer = fs.readFileSync(`${dir}/${e}`);
+        let img_tensor = tf.node.decodeImage(img_buffer);
+        let img_tensor_reshaped = img_tensor.resizeNearestNeighbor([512,512]);
+        tensors.push(img_tensor_reshaped);
+    });
+    return tensors
 }
 
-///sfasdfasdfs
+//mamba
 (async()=>{
-    let prik = await loadImages('./ok');
-    log(2, prik);
+    let mamba = await loadImages('./not_ok');
+    await log(0,mamba);
 })()
+
+//exports
+exports.loadImages = loadImages;
--- logger.js
+++ logger.js
@@ -2,11 +2,11 @@ function log(status, message){
   let dateObj = new Date();
   let date = `[${dateObj.getUTCHours()}:${dateObj.getUTCMinutes()}:${dateObj.getUTCSeconds()}]`;
   if (status == 0){
-    console.log("\x1b[1m\x1b[34m", `${date} ${message}`);
+    console.log("\x1b[1m\x1b[32m%s\x1b[0m", `${date}[INF] ${message}`);
   } else if (status == 1){
-    console.log("\x1b[1m\x1b[33m", `[WARN]${date} ${message}`);
+    console.log("\x1b[1m\x1b[33m%s\x1b[0m", `${date}[WARN] ${message}`);
   } else if (status == 2){
-    console.log("\x1b[4m\x1b[31m", `[ERR]${date} ${message}`);
+    console.log("\x1b[4m\x1b[31m%s\x1b[0m", `${date}[ERR] ${message}`);
   } else {
     console.log(`${date} ${message}`);
   }
