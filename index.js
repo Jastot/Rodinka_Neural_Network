c12b177608bfea75b5ce677b354bbedfed3815e1
andrik154 <a.u.litovets@gmail.com>
2021-07-06 17:48:39 +0300

    do a little trolling

--- index.js
+++ index.js
@@ -3,30 +3,30 @@ const fs = require('fs');
 const log = require('./logger.js').log; //custom logger xd
 const tf = require('@tensorflow/tfjs-node');
 
-//code
-var ok_unsorted = fs.readdirSync('./ok');
-var ok = [];
-
-var not_ok_unsorted = fs.readdirSync('./not_ok');
-var not_ok = [];
-
 //do a little trolling...
+
+//tezt 
 var img_buffer = fs.readFileSync('./not_ok/test.jpg');
 var img_tensor = tf.node.decodeImage(img_buffer);
+log(2, img_tensor.shape);
+var img_tensor2 = tf.image.resizeNearestNeighbor(img_tensor, [100,100]);
+log(2, img_tensor2.shape);
 log(0,img_tensor);
+
+//A LITTLE TROLLING
+async function loadImages(dir){
+    var unsorted = fs.readdirSync(dir);
+    var sorted = [];
+    await unsorted.forEach(e=>{
+        if(/(.+).png/.test(e)||/(.+).jpg/.test(e)||/(.+).jpeg/.test(e)){
+            sorted.push(e);
+        }
+    });
+    return sorted
+}
 
 ///sfasdfasdfs
 (async()=>{
-    await ok_unsorted.forEach(e=>{
-        if(/(.+).png/.test(e)||/(.+).jpg/.test(e)||/(.+).jpeg/.test(e)){
-            ok.push(e);
-        }
-    })
-    await not_ok_unsorted.forEach(e=>{
-        if(/(.+).png/.test(e)||/(.+).jpg/.test(e)||/(.+).jpeg/.test(e)){
-            not_ok.push(e);
-        }
-    })
-    console.log(ok);
-    console.log(not_ok);
-})();
+    let prik = await loadImages('./ok');
+    log(2, prik);
+})()
