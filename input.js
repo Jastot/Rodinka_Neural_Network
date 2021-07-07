ff7cccaf82bbc57ed60191fe7f5d5274e55dbe2f
andrik154 <a.u.litovets@gmail.com>
2021-07-07 16:18:30 +0300

    catsdogs

--- input.js
+++ input.js
@@ -6,22 +6,30 @@ const tf = require('@tensorflow/tfjs-node');
 //do a little trolling...
 
 //A LITTLE TROLLING
-function loadImages(dir){
+function loadImages(dir, limit = null){
+    log(0,`Loading images from ${__dirname+dir}`)
     var unsorted = fs.readdirSync(dir);
     var sorted = [];
     var tensors=[];
-    log(0,`Loading images from ${__dirname+dir}`)
+
     unsorted.forEach(e=>{
         if(/(.+).png/.test(e.toLowerCase())||/(.+).jpg/.test(e.toLowerCase())||/(.+).jpeg/.test(e.toLowerCase())){
-            sorted.push(e);
+            if(typeof(limit)=="number"){
+                if(sorted.length<limit){
+                    sorted.push(e);
+                }
+            } else {
+                sorted.push(e);
+            }
         }
     });
     sorted.forEach(e=>{
         let img_buffer = fs.readFileSync(`${dir}/${e}`);
         let img_tensor = tf.node.decodeImage(img_buffer)
-                            .resizeNearestNeighbor([128,128])
+                            .resizeNearestNeighbor([32,32])
                             .toFloat()
                             .div(tf.scalar(255.0))
+                            .expandDims();
         tensors.push(img_tensor);
     });
     log(0,"Loaded images");
@@ -38,8 +46,8 @@ function splitData(data){
 //mamba
 /*
 (async()=>{
-    log(3,loadImages('./not_ok'))
-})()*/
+    log(3,loadImages('./data/not_ok'))
+})()/*/
 
 //exports
-exports.laodImages = loadImages;
+exports.loadImages = loadImages;
--- model.js
+++ model.js
@@ -2,7 +2,29 @@
 const log = require('./logger.js').log;
 const tf = require('@tensorflow/tfjs-node');
 
+const loadImages = require('./input.js').loadImages;
+
 //model (custom)
 const model = tf.sequential({layers:[
-    tf.layers.dense({units:4, activation:'sigmoid', inputShape:[[2]]})
+    tf.layers.dense({units:4, activation:'sigmoid', inputShape:[200,]}),
+    tf.layers.dense({units:1, activation:'sigmoid'})
 ]});
+
+model.compile({
+    optimizer:tf.train.sgd('0.1'),
+    loss:tf.losses.meanSquaredError
+})
+
+const input_cat = loadImages('./data/train/cat', limit=100);
+const input_dog = loadImages('./data/train/dog', limit=100);
+const input = tf.concat([input_cat, input_dog]);
+const output = tf.concat([tf.ones([100]),tf.zeros([100])]);
+
+train().then(()=>{log(0, "Training complete")});
+
+async function train(){
+    for (let i = 0; i<10; i++){
+        const response = await model.fit(input, output);
+        log(3,response);
+    }
+}
